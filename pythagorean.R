50104b3c0b135942f59440972e6b9f5c46fc8909
Olivia <omleeson@gmail.com>
2016-10-24 20:30:06 -0400

    Commit function for Pythagorean Theorem

---
+++ pythagorean.R
@@ -0,0 +1,13 @@
+#Sum of Squares returns sum of x and y
+sum.of.squares <- function(x,y) {
+  z<- x^2 + y^2
+return(z)  
+}
+sum.of.squares(2,4)
+
+#Pythagorean Theorem
+hypotenuse <- function(x,y) {
+  z<- x^2 + y^2
+  h <- sqrt(z)
+  return(h)
+}
